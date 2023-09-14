18db4bb7b0e975b95e35d1f4b46a71a99c09817b
Danilo Giarlini <danilogiarlini@gmail.com>
2023-09-14 18:20:24 +0200

    first item added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import AccordionItem from './components/AccordionItem';
 
 function App() {
   return (
-    
+    <div className='container'>
+      <header>
+        <h2>Frequently Asked Questions</h2>
+        <p>Answers to common questions about our frontend challanges website. </p>
+      </header>
+      <main>
+        <AccordionItem />
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/AccordionItem.js
@@ -0,0 +1,11 @@
+import "./AccordionItem.css";
+
+const AccordionItem = () => {
+  return (
+    <div className="item">
+      <p>Some title</p>
+    </div>
+  );
+};
+
+export default AccordionItem;
